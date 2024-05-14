29ce56fc013cec9a31729b0e885ffe3d6a112519
Paul Ladd <vlendle@gmail.com>
2024-05-14 12:48:24 -0400

    Add basic socket setup

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io'
+
+const PORT = 3000
+const HOST = 'localhost'
+
 const app = express();
+const server = createServer(app)
+const io = new Server(server)
 
-app.get("/message", (_, res) => res.send("Hello from express!"));
+// Example route handler 
+//app.get("/message", (_, res) => res.send("Hello from express!"));
 
-ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+io.on('connection', socket => {
+  console.log("user connected")
+})
+
+server.listen(3000, () => {
+  console.log(`--- 🥷🧨 Emoji Assassin server up and running at ${HOST}:${PORT} 🥷🧨--- `)
+})
+
+
+ViteExpress.bind(app, server);
