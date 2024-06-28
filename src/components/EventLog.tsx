2eb548f6f972bfa39ec043e5aa7c5c1d0ed8fdac
Paul Ladd <vlendle@gmail.com>
2024-06-28 13:11:49 -0400

    Make event log collapsible, add styling

--- src/components/EventLog.tsx
+++ src/components/EventLog.tsx
@@ -1,23 +1,40 @@
 import { EventLogItem } from "@customTypes/events";
-
+import styles from "./EventLog.module.css"
+import { useState } from "react";
 interface EventLogProps {
   events: EventLogItem[]
 }
 
 export default function EventLog({ events }:EventLogProps) {
 
+  const [open, setOpen] = useState(false)
+
   const eventLogItems = events.map(({ message, timestamp }) => {
     return (
       <li key={timestamp}>
-        {message}
+        <span className={styles["timestamp"]}>{timestamp}</span>
+        <span className={styles["message"]}>{message}</span>
       </li>
     )
   })
 
+  const expandCollapseText = open ? "collapse" : "expand"
+
   return (
-    <section id={"event-log"}>
-      <h3>Event Log</h3>
-      <ul>
+    <section className={styles["event-log-wrapper"]}>
+      <h3 
+        className={styles["event-log-header"]}
+        onClick={() => setOpen(!open)}  
+      >
+        Event Log
+        <span className={styles["expand-collapse"]}> (click to {expandCollapseText})</span>
+      </h3>
+      <ul 
+        className={
+          styles["event-log-items"] + " " +
+          (open ? styles["open"] : "")
+        }
+      >
         {eventLogItems}
       </ul>
     </section>
