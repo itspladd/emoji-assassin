0144605a03a71ad0a9c323b8dffc834639af2a41
Paul Ladd <vlendle@gmail.com>
2024-06-28 12:32:03 -0400

    Update lint rules

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: ['./tsconfig.json', './tsconfig.node.json'],
+      tsconfigRootDir: __dirname,
+    },
+  }
+]
